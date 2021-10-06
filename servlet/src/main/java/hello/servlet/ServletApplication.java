f475f090ec0e7a557e3b1694b4502bcf70b21838
Your Name <p1230411@naver.com>
2021-10-07 08:49:52 +0900

    test

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServletApplication {
 
 	public static void main(String[] args) {
-		System.out.println('test');
+		System.out.println("test");
 		SpringApplication.run(ServletApplication.class, args);
 	}
 

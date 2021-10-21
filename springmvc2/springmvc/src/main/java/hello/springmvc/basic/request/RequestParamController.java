2029fdfb5464fceacc57d3da19b6ebe59d86a474
hsPark <p1230411@naver.com>
2021-10-21 17:26:45 +0900

    request-body-json-v1

---
+++ springmvc2/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,34 @@
+package hello.springmvc.basic.request;
+
+import hello.springmvc.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Slf4j
+public class RequestParamController {
+
+    @ResponseBody // class의 애노테이션이 @Controller 인데 해당 매서드의 return 값을 String으로 할 경우
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@RequestParam("username") String username, @RequestParam("age") int age){
+        HelloData hellodata = new HelloData();
+        hellodata.setUsername(username);
+        hellodata.setAge(age);
+
+        log.info("username = {}, age = {}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody // class의 애노테이션이 @Controller 인데 해당 매서드의 return 값을 String으로 할 경우
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(@ModelAttribute HelloData hellodata){
+        
+
+        log.info("username ={}, age = {}", hellodata.getUsername(), hellodata.getAge());
+        return "ok";
+    }
+}
